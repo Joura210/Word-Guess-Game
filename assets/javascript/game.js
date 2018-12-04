972077e1d2cf9ff433ccf0e6012377d6ac17f821
joura <martinezluisjorge86@gmail.com>
2018-12-04 13:26:24 -0500

    reset css/link bootstrap css/global variable add

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+//global variables
+
+var wins = 0;
+var losses = 0;
+var guessLeft = 9;
+// var guessSoFar = "";
+var compGuess = "";
+
+// changing our page to show outcomes
+
+var winText = document.getElementById("wins");
+var lossText = document.getElementById("losses");
+var guessLefttext = document.getElementById("guessLeft");
+var guessSoFartext = document.getElementById("guessSoFar");
