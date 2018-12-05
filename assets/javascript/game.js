db63f3cc43e2cbba4a51ac8aa1a5a68081f3e339
joura <martinezluisjorge86@gmail.com>
2018-12-05 15:15:39 -0500

    made array, logging users onkeys, made underscore array for possible future use.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,40 @@
 //global variables
 
-var wins = 0;
-var losses = 0;
-var guessLeft = 9;
-// var guessSoFar = "";
-var compGuess = "";
+var guessLeft = 12;
+var lettersUsed = "";
+// var compGuess = "";
+var underscores = [];
+var wrongGuess = "";
 
 // changing our page to show outcomes
 
-var winText = document.getElementById("wins");
-var lossText = document.getElementById("losses");
-var guessLefttext = document.getElementById("guessLeft");
-var guessSoFartext = document.getElementById("guessSoFar");
+var winTextwrongGuess = document.getElementById("wrongGuess");
+var lettersUsedtext = document.getElementById("lettersUsed");
+var underscorestext = document.getElementById("underscores");
+
+
+//Build our array to pull from, blank array for the user to fill
+var computerChoice = ["pikachu", "raichu", "bulbasaur", "ivysaur", "venusaur", "squirtle", "wartortle", "blastoise", "charmander", "charmeleon", "charizard"];
+var used = [];
+compGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+// console.log(compGuess)
+
+
+// generate compGuess with underscores
+
+var createUnderscores = () => {
+    for (var i= 0; i < computerChoice.length; i++){
+        underscores.push("_");
+    }
+return(underscores);
+
+}
+    // console.log(createUnderscores());
+
+
+
+// make player able to log choices with onkey up function
+document.onkeyup = function(event){
+    var userGuess = event.key;
+    // console.log(userGuess);
+}
